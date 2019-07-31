2d738d4eccd104ffbb8119e08da8f5ada0ec486a
Edmunds Steins <edja2915@gmail.com>
2019-07-31 11:42:52 +0100

    Initial working solution.

--- jasmine/src/Mergesort.js
+++ jasmine/src/Mergesort.js
@@ -1,20 +1,44 @@
 function Mergesort() {
 }
 
+
 Mergesort.prototype.sort = function(array) {
-    if (array.length < 2) {
+    if(array.length <= 1) {
         return [...array];
     }
     else {
-        // Step 1: divide into single elements. No real need, right?
-        // Step 2: sort each pair into an array and put into array.
-        return [...array];
+        var middle = array.length / 2;
+        var leftArray = this.sort(array.slice(0, middle));
+        var rightArray = this.sort(array.slice(middle));
+        return merge(leftArray, rightArray);
     }
 };
+
+function merge(leftArray, rightArray) {
+
+    var result = [];
+
+    while(leftArray.length !== 0 && rightArray.length !== 0) {
+        if(leftArray[0] <= rightArray[0]) {
+            result.push(leftArray.shift());
+        }
+        else {
+            result.push(rightArray.shift());
+        }
+    }
+
+    //If either array is empty, concatenate the other with the result.
+    if(leftArray.length === 0) {
+       result = result.concat(rightArray);
+    }
+    else {
+       result = result.concat(leftArray);
+    }
+
+    return result;
+}
 
 // Adapted from https://stackoverflow.com/questions/5836833/create-a-array-with-random-values
 Mergesort.prototype.randomArray = function(n) {
     return Array.from({length: n}, () => Math.floor(Math.random() * n));
 };
-
-console.log([2].toString() === [2].toString());
